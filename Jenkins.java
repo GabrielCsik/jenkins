6fa373c467535e2930d4a339cd23c68a098d7247
Gabriel Csík <gabriel.csik07@gmail.com>
2020-07-16 14:32:44 +0200

    second change from home pc

--- Jenkins.java
+++ Jenkins.java
@@ -1,6 +1,6 @@
 class Jenkins{
 	public static void main(String[] args){
 		System.out.println("Hello Jenkins");
-		System.out.println("change from other pc");
+		System.out.println("change number 2 from other pc");
 	}
 }
